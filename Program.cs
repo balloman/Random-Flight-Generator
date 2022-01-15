09730b786bd87c0e4df7b4b2c8168b6b90814310
Bernard Allotey <benard.allotey4@gmail.com>
2022-01-14 20:51:32 -0600

    Added some static methods that might fix our path issues

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-using System.Diagnostics;
-using System.Globalization;
+using Random_Realistic_Flight;
 using Random_Realistic_Flight.Services;
 using Random_Realistic_Flight.Services.Interfaces;
 
@@ -7,14 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.ConfigureForwardedHeaders();
 builder.Services.AddSingleton<IFlightService, AeroDataBoxService>();
 builder.Services.AddSingleton<IKeyService, PropertyKeySetter>();
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
-if (!builder.Environment.IsDevelopment())
-{
-    builder.Environment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
-}
 
 var app = builder.Build();
 
@@ -22,12 +18,10 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    // app.UseHsts();
-    
+    app.UseForwardedHeaders();
+    app.ConfigurePathBase();
 }
 
-// app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
---
+++ StartupExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.FileProviders;
+
+namespace Random_Realistic_Flight;
+
+public static class StartupExtensions
+{
+    public static void ConfigureForwardedHeaders(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+        });
+    }
+
+    public static void ConfigurePathBase(this WebApplication app)
+    {
+        var pathBase = Environment.GetEnvironmentVariable("ASPNETCORE_PATHBASE");
+        if (pathBase is null)
+        {
+            return;
+        }
+
+        if (!pathBase.StartsWith("/"))
+        {
+            pathBase = "/" + pathBase;
+        }
+
+        if (pathBase.EndsWith("/"))
+        {
+            pathBase = pathBase[..^1];
+        }
+        app.Use((context, next) =>
+        {
+            context.Request.PathBase = new PathString(pathBase);
+            return next(context);
+        });
+    }
+}
